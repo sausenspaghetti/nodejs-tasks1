21791540db0c37798951fb4989cc503cc108622d
sausenspaghetti <sausenspaghetti@gmail.com>
2023-02-27 12:42:36 +0600

    2nd task completed, if i understand it right

--- task2.js
+++ task2.js
@@ -2,16 +2,22 @@
     2.Написать модуль, который способен выполнять операции с числами любой длины.
     4 метода для сложения, умножения, вычитания и деления.
 */
-export { MyBigInt };
-// 1. Реализовать BigInt
-// 2. 
+
+export { sum, sub, div, mul };
 
 
+function sum(firstVal, secondVal) {
+    return BigInt(firstVal) + BigInt(secondVal);
+}
 
-class MyBigInt {
-    constructor(number) {}
-    add(number) {}
-    sub(number) {}
-    mul(number) {}
-    div(number) {}
+function sub(firstVal, secondVal) {
+    return BigInt(firstVal) - BigInt(secondVal);
+}
+
+function div(firstVal, secondVal) {
+    return BigInt(firstVal) / BigInt(secondVal);
+}
+
+function mul(firstVal, secondVal) {
+    return BigInt(firstVal) * BigInt(secondVal);
 }
